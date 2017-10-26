47deec77bb032767c3da0804532bdec384cfd733
DK\obr <obr@sitecore.net>
2017-10-26 11:18:26 +0300

    init commit

---
+++ src/Sitecore.Support.412550/TrimmingSearchQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ContentSearch.Pipelines.TranslateQuery
+{
+    using Sitecore.ContentSearch.Pipelines.TranslateQuery;
+    using Sitecore.ContentSearch.Utilities;
+    using System;
+    using System.Linq;
+
+    public class TrimmingSearchQuery : TranslateQueryProcessor
+    {
+        public override void Process(TranslateQueryArgs args)
+        {
+            if ((args != null) && (args.SearchStringModel != null))
+            {
+                for (int i = 0; i < args.SearchStringModel.Count<SearchStringModel>(); i++)
+                {
+                    if (args.SearchStringModel.ElementAt<SearchStringModel>(i).Type.StartsWith(" _"))
+                    {
+                        args.SearchStringModel.ElementAt<SearchStringModel>(i).Type = args.SearchStringModel.ElementAt<SearchStringModel>(i).Type.Trim();
+                    }
+                }
+            }
+        }
+    }
+}
